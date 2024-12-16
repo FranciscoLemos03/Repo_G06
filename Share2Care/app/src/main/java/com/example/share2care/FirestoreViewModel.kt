b1d5241f9cd612f038f306e5b1876f26d2285bad
Francisco Lemos <56840967+FranciscoLemos03@users.noreply.github.com>
2024-12-16 20:03:26 +0000

    Storage img to firestore
    adding front end and back end config to storage

--- Share2Care/app/src/main/java/com/example/share2care/FirestoreViewModel.kt
+++ Share2Care/app/src/main/java/com/example/share2care/FirestoreViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.share2care
 
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class FirestoreViewModel : ViewModel() {
 
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val storage: FirebaseStorage = FirebaseStorage.getInstance()
 
-    data class LojaSocialData(val nome: String, val descricao: String)
+    data class LojaSocialData(val nome: String, val descricao: String, val imagemUrl: String? = null)
 
     private val _lojaSocialData = MutableLiveData<LojaSocialData>()
     val lojaSocialData: LiveData<LojaSocialData> = _lojaSocialData
@@ -17,15 +22,12 @@ class FirestoreViewModel : ViewModel() {
     private val _firestoreState = MutableLiveData<FirestoreState>()
     val firestoreState: LiveData<FirestoreState> = _firestoreState
 
-    // Função para quando registar-mos um user criar os seguintes dados :
-    //   - Nome -> Nome da Loja Social
-    //   - Descrição -> Descrição da Loja Social
-    //   - Status -> Status da aprovação da conta
     fun saveLojaSocialToFirestore(uid: String) {
         val lojaSocialMap = mapOf(
             "nome" to "",
             "descricao" to "",
-            "status" to "Pendente"
+            "status" to "Pendente",
+            "imagemUrl" to ""
         )
 
         firestore.collection("lojaSocial").document(uid).set(lojaSocialMap)
@@ -38,10 +40,11 @@ class FirestoreViewModel : ViewModel() {
     }
 
     // Função para atualizar dados da Loja Social
-    fun updateLojaSocialDetails(uid: String, nome: String, descricao: String) {
+    fun updateLojaSocialDetails(uid: String, nome: String, descricao: String, imageURL: String) {
         val updatedData = mapOf(
             "nome" to nome,
-            "descricao" to descricao
+            "descricao" to descricao,
+            "imagemUrl" to imageURL
         )
 
         firestore.collection("lojaSocial").document(uid).update(updatedData)
@@ -53,14 +56,16 @@ class FirestoreViewModel : ViewModel() {
             }
     }
 
-    //Recolher dados da Loja Social
+
+
     fun getLojaSocialDetails(uid: String) {
         firestore.collection("lojaSocial").document(uid).get()
             .addOnSuccessListener { document ->
                 if (document.exists()) {
                     val nome = document.getString("nome") ?: ""
                     val descricao = document.getString("descricao") ?: ""
-                    _lojaSocialData.value = LojaSocialData(nome, descricao)
+                    val imagemUrl = document.getString("imagemUrl")
+                    _lojaSocialData.value = LojaSocialData(nome, descricao, imagemUrl)
                 } else {
                     _firestoreState.value = FirestoreState.Error("Loja Social não encontrada")
                 }
