49ef375356f25f48dc5b66e8f8d256f371db86e1
- RJ - <100219958+ruijor@users.noreply.github.com>
2025-01-07 19:45:06 +0000

    CheckIn and CheckOut
    backend do CheckIn/CheckOut

--- Share2Care/app/src/main/java/com/example/share2care/AppNavigation.kt
+++ Share2Care/app/src/main/java/com/example/share2care/AppNavigation.kt
@@ -149,8 +149,17 @@ fun AppNavigation(modifier: Modifier = Modifier,authViewModel: AuthViewModel){
         composable("checkin") {
             CheckInPage(navController,authViewModel)
         }
-        composable("checkout") {
-            CheckOutPage(navController,authViewModel)
+
+        composable(
+            route = "checkout/{beneficiarioID}/{nome}",
+            arguments = listOf(
+                navArgument("beneficiarioID") { type = NavType.StringType },
+                navArgument("nome") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val beneficiarioID = backStackEntry.arguments?.getString("beneficiarioID") ?: ""
+            val nome = backStackEntry.arguments?.getString("nome") ?: ""
+            CheckOutPage(beneficiarioID, nome, navController, authViewModel)
         }
     })
 }
